94c8fd2734b8e6bcb4e0c03ccb6d44afe78c4fe5
GabrielDevss <gabriel0211oliveira15@gmail.com>
2022-02-22 14:45:42 -0300

    ignews

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -1,11 +1,15 @@
 import { FaGithub} from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const { data: session} = useSession();
+
+  console.log(session);
+
+  return session ? (
     <button 
     type="button"
     className={styles.signButton}
@@ -22,6 +26,7 @@ export function SignButton() {
     <button 
     type="button"
     className={styles.signButton}
+    onClick={() => signIn('github')}
     >
       <FaGithub color="#EBA417" />
       Sing in with GitHub
